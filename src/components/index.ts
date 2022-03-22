17f84d76e22005f5b508615225e89b62ac572965
nicoheredia59 <nicoheredia59@gmail.com>
2022-03-21 23:39:12 -0300

    adding new exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,7 @@
 export { default as Button } from "./Button";
 export { default as ColorProvider } from "./Provider";
-export { default as Stack } from "./Containers/Stack";
+export { default as Stack } from "./Containers";
 export { default as Form } from "./Form";
 export { default as Input } from "./Input";
+export { default as AsyncLoader } from "./AsyncLoader/Loader/AsyncLoader";
+export { default as AsyncItem } from "./AsyncLoader/Item/AsyncItem";
