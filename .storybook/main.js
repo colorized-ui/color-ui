f78befbf3de07a9b605a2fd170dd5b2a0144a1f3
nicoheredia59 <nicoheredia59@gmail.com>
2022-03-24 21:49:04 -0300

    renaming package to src for bugs

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,5 @@
 module.exports = {
-  stories: [
-    "../package/**/*.stories.mdx",
-    "../package/**/*.stories.@(js|jsx|ts|tsx)",
-  ],
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
--- rollup.config.js
+++ rollup.config.js
@@ -9,7 +9,7 @@ const packageJson = require("./package.json");
 
 export default [
   {
-    input: "package/index.ts",
+    input: "src/index.ts",
     output: [
       {
         file: packageJson.main,
